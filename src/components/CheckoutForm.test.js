92316d9f72547fd99c6397827ae5643b33d4328a
luisbryandr <luisbdominguezr@gmail.com>
2021-10-29 15:00:02 -0400

    completed Testing requirements, supplied different ways to test for desired outcome

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -9,4 +10,27 @@ test("renders without errors", () => {
     render(<CheckoutForm />);
 });
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", async () => {
+    //Arrange: set up for success, render component were working with
+    render(<CheckoutForm />)
+
+    //Act:select the element(s) and actions/events in question to work with
+    const submitButton = screen.getByRole('button');//select our submit button
+    userEvent.click(submitButton);//simulate user clicking out button
+
+    const success = await screen.findByTestId('successMessage')// this waits for user to click the submit button then finds the element with the test-id of successMessage as an attribute
+
+    
+
+    //Assert: verify that said elements and events are working as expected
+    expect(success).toBeInTheDocument();// this checks that the successmessage element is in the document
+    expect(success).toBeTruthy();//alternative way of making sure success message is dsiplaying
+    expect(success).toBeVisible(); //checks if the elment is seen on the page
+    // expect(success).toHaveLength(1); doesnt work because we are not getting an array back
+
+    //ALTERNATIVE METHOD - lends more to manually break so i can verify check is really working
+    // const success2 = await screen. getByText(/you have ordered some plants/i)//partial match to look for string of success message
+    // expect(success2).toBeInTheDocument();
+
+   
+});
