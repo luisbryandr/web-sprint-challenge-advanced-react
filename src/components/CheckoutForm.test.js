00d549983d4249a173eb9b2978c4e0cab1ece14e
luisbryandr <luisbdominguezr@gmail.com>
2021-10-29 15:32:46 -0400

    answering interview questions - also started on custom hook

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -18,6 +18,8 @@ test("shows success message on submit with form details", async () => {
     const submitButton = screen.getByRole('button');//select our submit button
     userEvent.click(submitButton);//simulate user clicking out button
 
+    
+
     const success = await screen.findByTestId('successMessage')// this waits for user to click the submit button then finds the element with the test-id of successMessage as an attribute
 
     
@@ -32,5 +34,6 @@ test("shows success message on submit with form details", async () => {
     // const success2 = await screen. getByText(/you have ordered some plants/i)//partial match to look for string of success message
     // expect(success2).toBeInTheDocument();
 
-   
+   // still missing making sure form details are also in the success message
+    // even though the code and app isnt validating the form input, just updating with whatver values are insert and rendering in the success message so i dont need to test for that.  as far as i can tell my test check for successmessage rendering (with form details -even if they are empty- since im targeting the parent element (the div holding the text))
 });
