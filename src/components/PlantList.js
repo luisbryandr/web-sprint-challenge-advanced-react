705bc57a046b5fab3a692a86ae863d49c0a61ae1
luisbryandr <luisbdominguezr@gmail.com>
2021-10-29 14:35:27 -0400

    completed Plant List component (fetching plant data on mount)

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -6,6 +6,17 @@ export default class PlantList extends Component {
   state = {
     plants:[]
   }
+
+  componentDidMount(){
+    axios.get("http://localhost:3333/plants")
+    .then(res=>{
+      this.setState({
+        ...this.state,
+      plants: res.data}
+    )}).catch(err=>{
+      console.log(err)
+    });
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
